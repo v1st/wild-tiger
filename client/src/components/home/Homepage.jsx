1ba281e4e75ae1b42e91f79bfed89cfac7ec1a32
v1st <brandon_m918@yahoo.com>
2019-01-12 21:03:07 -0500

    added header banner

--- client/src/components/home/Homepage.jsx
+++ client/src/components/home/Homepage.jsx
@@ -1,11 +1,27 @@
 import React, { Component } from 'react'
+import '../../scss/home.scss';
 
 class Homepage extends Component {
   render() {
     return (
-      <div>
+      <React.Fragment>
+      <div className="homepage__container">
+        <div className="header__image"></div>
+        <div className="header-info__container">
+          <div className="header-info__title-wrap">
+            <div className="header-info__title">Tasty Thai Food</div>
+            <span className="line-break"></span>
+          </div>
+          <div className="header-info__quote">"Delicious dishes from fresh ingredients"</div>
+          <button className="order__button">Order Now</button>
+        </div>
+      <div className="header__banner">
+      
+      </div>
 
       </div>
+      </React.Fragment>
+
     )
   }
 }
